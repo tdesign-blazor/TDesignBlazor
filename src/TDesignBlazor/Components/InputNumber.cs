0edd08205a428cac8b0566e1788befc99389125d
吴新恒 <17629957093@163.com>
2022-10-11 19:07:41 +0800

    新建inputNumber组件

---
+++ src/TDesignBlazor/Components/InputNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDesignBlazor.Components
+{
+    [HtmlTag("div")]
+    [CssClass("t-input-number  t-size-m t-input-number--row")]
+    public class InputNumber : BlazorComponentBase
+    {
+        
+    }
+}
