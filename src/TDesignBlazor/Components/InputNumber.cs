0c428a3b9c5d7439aaaf1d0f65c9af50cf776d37
吴新恒 <17629957093@163.com>
2022-10-11 23:34:01 +0800

    新建文档

--- src/TDesignBlazor/Components/InputNumber.cs
+++ src/TDesignBlazor/Components/InputNumber.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,12 @@ namespace TDesignBlazor.Components
     [CssClass("t-input-number  t-size-m t-input-number--row")]
     public class InputNumber : BlazorComponentBase
     {
-        
+
+        protected override void AddContent(RenderTreeBuilder builder, int sequence)
+        {
+            builder.CreateComponent<Button>(sequence + 1);
+            builder.CreateComponent<Input<int>>(sequence + 2);
+            builder.CreateComponent<Button>(sequence + 3);
+        }
     }
 }
