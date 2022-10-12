ee35a9222c23c58ffcd30477bbbe012ee2dfd104
Zhou Zihao <william-zzh@outlook.com>
2022-10-12 13:37:05 +0800

    增加带刻度的显示

--- src/TDesignBlazor.Test/Components/SliderTest.cs
+++ src/TDesignBlazor.Test/Components/SliderTest.cs
@@ -1,16 +1,20 @@
-﻿namespace TDesignBlazor.Test.Components
+﻿using Microsoft.AspNetCore.Components;
+using OneOf;
+using System.Reflection.Emit;
+
+namespace TDesignBlazor.Test.Components
 {
-    public class SliderTest:TestBase<Slider>
+    public class SliderTest : TestBase<Slider>
     {
-        [Fact(DisplayName ="Slider - 默认渲染值是12的滑块")]
+        [Fact(DisplayName = "Slider - 默认渲染值是12的滑块")]
         public void Test_Render_With_Value_Is_12()
         {
             var markup = @"
 <div class=""t-slider__container"" aria-valuetext=""12"">
     <div role=""slider"" aria-valuemin=""0"" aria-valuemax=""100"" aria-orientation=""horizontal"" class=""t-slider"">
         <div class=""t-slider__rail"">
-            <div class=""t-slider__track"" style=""width:12%;left:0%;""></div>
-            <div tabindex=""0"" show-tooltip=""true"" class=""t-slider__button-wrapper"" style=""left:12%;"">
+            <div class=""t-slider__track"" style=""width:12%;left:0%""></div>
+            <div tabindex=""0"" show-tooltip=""true"" class=""t-slider__button-wrapper"" style=""left:12%"">
                 <div class=""t-slider__button""></div>
             </div>
         </div>
@@ -20,12 +24,49 @@
             GetComponent(m => m.Add(p => p.Value, 12)).MarkupMatches(markup);
         }
 
-        [Fact(DisplayName ="Slider - 默认渲染值是 30,70 的滑块")]
+        [Fact(DisplayName = "Slider - 默认渲染值是 30,70 的滑块")]
         public void Test_Render_With_Value_Is_30_70()
         {
             var markup = @"
 <div class=""t-slider__container"" aria-valuetext=""30-70"">
     <div role=""slider"" aria-valuemin=""0"" aria-valuemax=""100"" aria-orientation=""horizontal"" class=""t-slider"">
+        <div class=""t-slider__rail"">
+            <div class=""t-slider__track"" style=""width:40%;left:30%""></div>
+            <div tabindex=""0"" show-tooltip=""true"" class=""t-slider__button-wrapper"" style=""left:30%"">
+                <div class=""t-slider__button""></div>
+            </div>
+            <div tabindex=""0"" show-tooltip=""true"" class=""t-slider__button-wrapper"" style=""left:70%"">
+                <div class=""t-slider__button""></div>
+            </div>
+        </div>
+    </div>
+</div>
+";
+            GetComponent(m => m.Add(p => p.Value, (30, 70))).MarkupMatches(markup);
+        }
+
+
+        [Fact(DisplayName = "Slider - 垂直显示")]
+        public void Test_Vertical()
+        {
+            var markup1 = @"
+<div class=""t-slider__container is-vertical"" aria-valuetext=""12"">
+    <div role=""slider"" aria-valuemin=""0"" aria-valuemax=""100"" aria-orientation=""vertical"" class=""t-slider is-vertical t-slider--vertical"">
+        <div class=""t-slider__rail"">
+            <div class=""t-slider__track"" style=""width:12%;left:0%;""></div>
+            <div tabindex=""0"" show-tooltip=""true"" class=""t-slider__button-wrapper"" style=""left:12%;"">
+                <div class=""t-slider__button""></div>
+            </div>
+        </div>
+    </div>
+</div>
+";
+            GetComponent(m => m.Add(p => p.Value, 12).Add(p => p.Vertical, true)).MarkupMatches(markup1);
+
+
+            var markup2 = @"
+<div class=""t-slider__container is-vertical"" aria-valuetext=""30-70"">
+    <div role=""slider"" aria-valuemin=""0"" aria-valuemax=""100"" aria-orientation=""vertical"" class=""t-slider is-vertical t-slider--vertical"">
         <div class=""t-slider__rail"">
             <div class=""t-slider__track"" style=""width:40%;left:30%;""></div>
             <div tabindex=""0"" show-tooltip=""true"" class=""t-slider__button-wrapper"" style=""left:30%;"">
@@ -38,7 +79,137 @@
     </div>
 </div>
 ";
-            GetComponent(m => m.Add(p => p.Value, (30,70))).MarkupMatches(markup);
+            GetComponent(m => m.Add(p => p.Value, (30, 70)).Add(p => p.Vertical, true)).MarkupMatches(markup2);
+        }
+
+        [Fact(DisplayName = "Slider - 禁用状态")]
+        public void Test_Disabled()
+        {
+            var markup1 = @"
+<div class=""t-slider__container"" aria-valuetext=""12"">
+    <div role=""slider"" aria-valuemin=""0"" aria-valuemax=""100"" aria-orientation=""horizontal"" class=""t-slider t-is-disabled"">
+        <div class=""t-slider__rail t-is-disabled"">
+            <div class=""t-slider__track"" style=""width:12%;left:0%;""></div>
+            <div tabindex=""0"" show-tooltip=""true"" class=""t-slider__button-wrapper"" disabled=""disabled"" style=""left:12%;"">
+                <div class=""t-slider__button""></div>
+            </div>
+        </div>
+    </div>
+</div>
+";
+            GetComponent(m => m.Add(p => p.Value, 12).Add(p => p.Disabled, true)).MarkupMatches(markup1);
+
+
+            var markup2 = @"
+<div class=""t-slider__container"" aria-valuetext=""30-70"">
+    <div role=""slider"" aria-valuemin=""0"" aria-valuemax=""100"" aria-orientation=""horizontal"" class=""t-slider  t-is-disabled"">
+        <div class=""t-slider__rail t-is-disabled"">
+            <div class=""t-slider__track"" style=""width:40%;left:30%;""></div>
+            <div tabindex=""0"" show-tooltip=""true"" class=""t-slider__button-wrapper"" disabled=""disabled"" style=""left:30%;"">
+                <div class=""t-slider__button""></div>
+            </div>
+            <div tabindex=""0"" show-tooltip=""true"" class=""t-slider__button-wrapper"" disabled=""disabled"" style=""left:70%;"">
+                <div class=""t-slider__button""></div>
+            </div>
+        </div>
+    </div>
+</div>
+";
+            GetComponent(m => m.Add(p => p.Value, (30, 70)).Add(p => p.Disabled, true)).MarkupMatches(markup2);
+        }
+
+        [Fact(DisplayName = "Slider - 带刻度 1 个 Value 值")]
+        public void Test_Marks_With_Single_Value()
+        {
+            var markup1 = @"
+<div class=""t-slider__container"" aria-valuetext=""12"">
+    <div role=""slider"" aria-valuemin=""0"" aria-valuemax=""100"" aria-orientation=""horizontal"" class=""t-slider"">
+        <div class=""t-slider__rail"">
+            <div class=""t-slider__track"" style=""width:12%;left:0%;""></div>
+            <div tabindex=""0"" show-tooltip=""true"" class=""t-slider__button-wrapper"" style=""left:12%;"">
+                <div class=""t-slider__button""></div>
+            </div>
+            <div>
+                <div>
+                    <div class=""t-slider__stop t-slider__mark-stop"" style=""left:0%;""></div>
+                    <div class=""t-slider__stop t-slider__mark-stop"" style=""left:20%;""></div>
+                    <div class=""t-slider__stop t-slider__mark-stop"" style=""left:40%;""></div>
+                    <div class=""t-slider__stop t-slider__mark-stop"" style=""left:60%;""></div>
+                    <div class=""t-slider__stop t-slider__mark-stop"" style=""left:80%;""></div>
+                    <div class=""t-slider__stop t-slider__mark-stop"" style=""left:100%;""></div>
+                </div>
+                <div class=""t-slider__mark"">
+                    <div class=""t-slider__mark-text"" style=""left:0%;"">0°C</div>
+                    <div class=""t-slider__mark-text"" style=""left:20%;"">20°C</div>
+                    <div class=""t-slider__mark-text"" style=""left:40%;"">40°C</div>
+                    <div class=""t-slider__mark-text"" style=""left:60%;"">60°C</div>
+                    <div class=""t-slider__mark-text"" style=""left:80%;"">80°C</div>
+                    <div class=""t-slider__mark-text"" style=""left:100%;"">100°C</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+";
+            GetComponent(m => m.Add(p => p.Value, 12).Add(p => p.Marks, new Dictionary<double, OneOf<string?, RenderFragment?, MarkupString?>>
+            {
+                [0D] = "0°C",
+                [20D] = "20°C",
+                [40D] = "40°C",
+                [60D] = "60°C",
+                [80D] = "80°C",
+                [100D] = "100°C"
+            })).MarkupMatches(markup1);
+
+
+        }
+
+        [Fact(DisplayName = "Slider - 带刻度的 2 个 Value 值")]
+        public void Test_Marks_With_Two_Value()
+        {
+
+            var markup2 = @"
+<div class=""t-slider__container"" aria-valuetext=""30-70"">
+    <div role=""slider"" aria-valuemin=""0"" aria-valuemax=""100"" aria-orientation=""horizontal"" class=""t-slider "">
+        <div class=""t-slider__rail"">
+            <div class=""t-slider__track"" style=""width:40%;left:30%;""></div>
+            <div tabindex=""0"" show-tooltip=""true"" class=""t-slider__button-wrapper"" style=""left:30%;"">
+                <div class=""t-slider__button""></div>
+            </div>
+            <div tabindex=""0"" show-tooltip=""true"" class=""t-slider__button-wrapper"" style=""left:70%;"">
+                <div class=""t-slider__button""></div>
+            </div>
+            <div>
+                <div>
+                    <div class=""t-slider__stop t-slider__mark-stop"" style=""left:0%;""></div>
+                    <div class=""t-slider__stop t-slider__mark-stop"" style=""left:20%;""></div>
+                    <div class=""t-slider__stop t-slider__mark-stop"" style=""left:40%;""></div>
+                    <div class=""t-slider__stop t-slider__mark-stop"" style=""left:60%;""></div>
+                    <div class=""t-slider__stop t-slider__mark-stop"" style=""left:80%;""></div>
+                    <div class=""t-slider__stop t-slider__mark-stop"" style=""left:100%;""></div>
+                </div>
+                <div class=""t-slider__mark"">
+                    <div class=""t-slider__mark-text"" style=""left:0%;"">0°C</div>
+                    <div class=""t-slider__mark-text"" style=""left:20%;"">20°C</div>
+                    <div class=""t-slider__mark-text"" style=""left:40%;"">40°C</div>
+                    <div class=""t-slider__mark-text"" style=""left:60%;"">60°C</div>
+                    <div class=""t-slider__mark-text"" style=""left:80%;"">80°C</div>
+                    <div class=""t-slider__mark-text"" style=""left:100%;"">100°C</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+";
+            GetComponent(m => m.Add(p => p.Value, (30, 70)).Add(p => p.Marks, new Dictionary<double, OneOf<string?, RenderFragment?, MarkupString?>>
+            {
+                [0D] = "0°C",
+                [20D] = "20°C",
+                [40D] = "40°C",
+                [60D] = "60°C",
+                [80D] = "80°C",
+                [100D] = "100°C"
+            })).MarkupMatches(markup2);
         }
     }
 }
--- src/TDesignBlazor/Components/Slider.cs
+++ src/TDesignBlazor/Components/Slider.cs
@@ -12,7 +12,7 @@ namespace TDesignBlazor;
 /// 滑块（滑动型输入器），是帮助用户在连续或间断的区间内，通过滑动来选择合适数值（一个数值或范围数值）的控件。
 /// </summary>
 [CssClass("t-slider__container")]
-public class Slider : BlazorComponentBase, IHasTwoWayBinding<OneOf<double, (double min,double max)>>
+public class Slider : BlazorComponentBase, IHasTwoWayBinding<OneOf<double, (double min,double max)>>,IHasDisabled
 {
     /// <summary>
     /// 设置滑块的最小值。
@@ -25,7 +25,11 @@ public class Slider : BlazorComponentBase, IHasTwoWayBinding<OneOf<double, (doub
     /// <summary>
     /// 是否垂直显示。
     /// </summary>
-    [Parameter]public bool Vertical { get; set; }
+    [Parameter][CssClass("is-vertical")]public bool Vertical { get; set; }
+
+    /// <inheritdoc/>
+    [Parameter]public bool Disabled { get; set; }
+    [Parameter] public Dictionary<double, OneOf<string?,RenderFragment?,MarkupString?>> Marks { get; set; } = new();
     /// <inheritdoc/>
     [Parameter]public OneOf<double, (double min, double max)> Value { get; set; }
     /// <inheritdoc/>
@@ -33,54 +37,100 @@ public class Slider : BlazorComponentBase, IHasTwoWayBinding<OneOf<double, (doub
     /// <inheritdoc/>
     [Parameter]public EventCallback<OneOf<double, (double min, double max)>>? ValueChanged { get; set; }
 
-
+    /// <summary>
+    /// 获取进度条的宽度百分比。
+    /// </summary>
     double Width
     {
         get
         {
-            if ( Value.IsT0 )
-            {
-                return Left1;
-            }
-            var width = Left2 - Left1;
+            var width = MaxValue - MinValue;
             if ( width <= 0 )
             {
-                return Left1;
+                return MinValue;
             }
             return width;
         }
     }
-    double Left1
+
+    /// <summary>
+    /// 表示最小值的百分比。
+    /// </summary>
+    double MinValue
     {
         get
         {
-            if ( Value.IsT1 )
+            if ( IsSingleNumer )
             {
-                return Value.AsT1.min;
+                return 0;
             }
-            return 0;
+            return Value.AsT1.min;
         }
     }
-    double Left2
+    /// <summary>
+    /// 表示最大值的百分比
+    /// </summary>
+    double MaxValue
     {
         get
         {
-            if ( Value.IsT1 )
+            if ( IsSingleNumer )
             {
-                return Value.AsT1.max;
+                return Value.AsT0;
             }
-            return Max;
+            return Value.AsT1.max;
         }
     }
+
+    /// <summary>
+    /// 获取一个布尔值，表示 Value 只有一个数
+    /// </summary>
+    bool IsSingleNumer => Value.IsT0;
 
     /// <inheritdoc/>
 
     protected override void OnParametersSet()
     {
         base.OnParametersSet();
-        if(Min<0|| Max>100 || Max < Min )
+        if ( Min < 0 || Max > 100 || Max < Min )
         {
             throw new InvalidOperationException($"{nameof(Min)} 不能小于 0 或 {nameof(Max)} 不能大于 100 并且 {nameof(Min)} 不能大于 {nameof(Max)}");
+        }
+
+        if ( IsSingleNumer )
+        {
+            var value=Value.AsT0;
+            if(value<Min || value > Max )
+            {
+                throw new InvalidOperationException($"参数 {nameof(Value)} 的值({value})必须在{Min}-{Max}之间");
+            }
+        }
+        else
+        {
+            var min = Value.AsT1.min;
+            var max = Value.AsT1.max;
+            if(min<Min || max > Max || min>max )
+            {
+                throw new InvalidOperationException($"参数 {nameof(Value)} 的值({min},{max})必须在{Min}-{Max}之间，并且第一个值不能小于{Min}，第二个值不能大于{Max}，第一个值不能大于第二个值");
+            }
+        }
+
+        if ( Marks is null )
+        {
+            throw new InvalidOperationException($"参数 {nameof(Marks)} 是 null");
+        }
+
+        if ( Marks.Any() )
+        {
+            if ( !Marks.TryGetValue(Min,out _) )
+            {
+                throw new InvalidOperationException($"{nameof(Marks)} 的必须有一个最小值，且必须等于 {nameof(Min)} 参数的值");
+            }
+
+            if ( !Marks.TryGetValue(Max, out _) )
+            {
+                throw new InvalidOperationException($"{nameof(Marks)} 的必须有一个最大值，且必须等于 {nameof(Max)} 参数的值");
+            }
         }
     }
 
@@ -94,23 +144,29 @@ public class Slider : BlazorComponentBase, IHasTwoWayBinding<OneOf<double, (doub
                 rail.CreateElement(0, "div", attributes: new
                 {
                     @class = "t-slider__track",
-                    style = HtmlHelper.CreateStyleBuilder().Append($"width:{Width}%").Append($"left:{Left1}%")
+                    style = HtmlHelper.CreateStyleBuilder().Append($"width:{Width}%").Append($"left:{MinValue}%")
                 });
 
-                BuildButtonWarpper(rail, 0, Left1);
-                if ( Value.IsT1 )
+                if ( !IsSingleNumer )
                 {
-                    BuildButtonWarpper(rail, 1, Left2);
+                    BuildButtonWarpper(rail, 0, MinValue);
                 }
 
-            }, new { @class = "t-slider__rail" });
+                BuildButtonWarpper(rail, 1, MaxValue);
+
+                BuildMarks(rail, 2);
+            }, 
+            new
+            {
+                @class = HtmlHelper.CreateCssBuilder().Append("t-slider__rail").Append("t-is-disabled", Disabled)
+            });
         }, 
         new { 
             role="slider",
             aria_valuemin= Min,
             aria_valuemax= Max,
             aria_orientation= Vertical ? "vertical": "horizontal",
-            @class = "t-slider",
+            @class = HtmlHelper.CreateCssBuilder().Append("t-slider").Append("is-vertical t-slider--vertical",Vertical).Append("t-is-disabled",Disabled),
         });
     }
 
@@ -129,7 +185,51 @@ public class Slider : BlazorComponentBase, IHasTwoWayBinding<OneOf<double, (doub
             tabindex = 0,
             show_tooltip = "true",
             @class = "t-slider__button-wrapper",
-            style = $"left:{value}%"
+            style = $"left:{value}%",
+            disabled = Disabled,
+        });
+    }
+
+    private void BuildMarks(RenderTreeBuilder builder,int sequence)
+    {
+        if ( !Marks.Any() )
+        {
+            return;
+        }
+
+        builder.CreateElement(sequence, "div", content =>
+        {
+            content.CreateElement(0, "div", value =>
+            {
+                int index = 0;
+                foreach ( var item in Marks.Keys )
+                {
+                    value.CreateElement(index, "div", attributes: new
+                    {
+                        @class = "t-slider__stop t-slider__mark-stop",
+                        style = $"left:{item}%;"
+                    });
+                    index++;
+                }
+            });
+
+            content.CreateElement(1, "div", text =>
+            {
+                int index = 0;
+                foreach ( var item in Marks )
+                {
+                    text.CreateElement(index, "div",item.Value, attributes: new
+                    {
+                        @class = "t-slider__mark-text",
+                        style = $"left:{item.Key}%;"
+                    });
+                    index++;
+                }
+            }, 
+            new
+            {
+                @class="t-slider__mark"
+            });
         });
     }
 }
