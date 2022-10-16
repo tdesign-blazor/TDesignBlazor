45b84936317dd2de54629672dfe6659674f15627
吴新恒 <17629957093@163.com>
2022-10-16 21:53:01 +0800

    完成inputnumber 部分功能，剩余功能待确认

--- src/TDesign/Components/Forms/InputNumber.cs
+++ src/TDesign/Components/Forms/InputNumber.cs
@@ -1,6 +1,10 @@
-﻿using System.Linq.Expressions;
-
+﻿using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Net.Http;
+using ComponentBuilder;
 using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.JSInterop;
 
 namespace TDesign;
 /// <summary>
@@ -13,6 +17,7 @@ namespace TDesign;
 [CssClass("t-input-number")]
 public class TInputNumber<TValue> : BlazorComponentBase, IHasTwoWayBinding<TValue>
 {
+
     readonly static Type[] SupportTypes = new[] { typeof(int), typeof(short), typeof(long), typeof(decimal), typeof(double), typeof(float) };
     public TInputNumber()
     {
@@ -21,47 +26,132 @@ public class TInputNumber<TValue> : BlazorComponentBase, IHasTwoWayBinding<TValu
             throw new NotSupportedException($"{nameof(TValue)} 仅支持 short, int, long, decimal, double 和 float 类型。");
         }
     }
-
-    [Parameter] public Size Size { get; set; } = Size.Medium;
+    /// <summary>
+    /// 大小
+    /// </summary>
+    [Parameter][CssClass] public Size Size { get; set; } = Size.Medium;
+    /// <summary>
+    /// 值，仅支持 short, int, long, decimal, double 和 float 类型。
+    /// </summary>
     [Parameter] public TValue? Value { get; set; }
     [Parameter] public Expression<Func<TValue?>>? ValueExpression { get; set; }
     [Parameter] public EventCallback<TValue?>? ValueChanged { get; set; }
+    /// <summary>
+    /// 占位符
+    /// </summary>
     [Parameter] public string? Placeholder { get; set; }
-
+    /// <summary>
+    /// 对齐方式
+    /// </summary>
+    [Parameter] public HorizontalAlignment Align { get; set; } = HorizontalAlignment.Center;
+    /// <summary>
+    /// 步救
+    /// </summary>
+    [Parameter] public TValue? Step { get; set; } = (TValue)Convert.ChangeType(1, typeof(TValue));
+    /// <summary>
+    /// 最大值，
+    /// </summary>
+    [Parameter] public TValue? Max { get; set; } = (TValue)Convert.ChangeType(int.MaxValue, typeof(TValue));
+    /// <summary>
+    /// 类型
+    /// </summary>
+    [Parameter][CssClass("t-input-number--")] public InputNumberTheme Theme { get; set; } = InputNumberTheme.Row;
+    /// <summary>
+    /// 自适应宽度
+    /// </summary>
+    [Parameter][CssClass("t-input-number--auto-width")] public bool AutoWidth { get; set; }
+    /// <summary>
+    /// 状态。
+    /// </summary>
+    [Parameter][CssClass("t-is-")] public Status Status { get; set; } = Status.Default;
+    /// <summary>
+    /// 输入框后缀显示的文本。
+    /// </summary>
+    [Parameter] public string? SuffixText { get; set; }
+    /// <summary>
+    /// 标签
+    /// </summary>
+    [Parameter] public string? Label { get; set; }
     protected override void AddContent(RenderTreeBuilder builder, int sequence)
     {
-        BuildButton(builder, sequence + 1, IconName.Remove);
+        dynamic _value = Value;
+        dynamic _step = Step;
+        dynamic _max = Max;
+        var _disabled = Max != null ? (bool)(_value >= _max) : false;
+
+        BuildButton(builder, sequence + 1, IconName.Remove, false, Theme != InputNumberTheme.Normal, a =>
+        {
+            Value = (TValue)(_value - _step);
+        });
         builder.CreateComponent<TInputText<TValue>>(sequence + 2, attributes: new
         {
             Value,
             ValueExpression,
             ValueChanged,
             Size,
-            Placeholder
+            Placeholder,
+            Alignment = Align,
+            Status = _disabled ? Status.Error : Status,
+            SuffixText = SuffixText,
+            PrefixText = Label,
+            oninput = "OnInput",
+            onkeydown = HtmlHelper.CreateCallback<KeyboardEventArgs>(this, e =>
+            {
+                if (e.Key == "ArrowUp"&&! _disabled)
+                    Value = (TValue)(_value + _step);
+                else if (e.Key == "ArrowDown")
+                    Value = (TValue)(_value - _step);
+
+            }),
+            onfocus = "@OnFocusAsync",
+            onblur = "@OnBlurAsync",
+            AutoWidth,
         });
-        BuildButton(builder, sequence + 3, IconName.Add);
+        BuildButton(builder, sequence + 3, IconName.Add, _disabled, Theme != InputNumberTheme.Normal, a =>
+        {
+            Value = (TValue)(_value + _step);
+        });
+
     }
 
-    protected override void BuildCssClass(ICssClassBuilder builder)
-    {
-        builder.Append("t-input-number--row");
-    }
-
-    private void BuildButton(RenderTreeBuilder builder, int sequence, object iconName, Action<MouseEventArgs>? click = default)
+    private void BuildButton(RenderTreeBuilder builder, int sequence, object iconName, bool disabled, bool condition, Action<MouseEventArgs>? click = default)
     {
         builder.CreateComponent<TButton>(sequence + 1, content =>
         {
             content.CreateComponent<TIcon>(0, attributes: new
             {
                 Name = iconName,
-                onclick = HtmlHelper.CreateCallback<MouseEventArgs>(this, e => click?.Invoke(e))
             });
         },
         new
         {
             Varient = ButtonVarient.Outline,
-            Shape = ButtonShape.Square
-        });
+            Shape = ButtonShape.Square,
+            @Class = HtmlHelper.CreateCssBuilder()
+            .Append($"t-input-number__decrease", iconName.ToString() == IconName.Remove.ToString())
+            .Append($"t-input-number__increase", iconName.ToString() == IconName.Add.ToString())
+            .Append($"t-is-disabled", iconName.ToString() == IconName.Add.ToString() && disabled),
+            Onclick = HtmlHelper.CreateCallback<MouseEventArgs>(this, e => click?.Invoke(e)),
+            Disabled = disabled,
+            
+        }, condition);
     }
+
+   
 }
 
+public enum InputNumberTheme
+{
+    /// <summary>
+    /// 按钮横向排列
+    /// </summary>
+    Row,
+    /// <summary>
+    /// 按钮行内纵向排列
+    /// </summary>
+    [CssClass("column t-is-controls-right ")] Column,
+    /// <summary>
+    /// 没有按钮，通过上下键控制
+    /// </summary>
+    Normal
+}
