87983a8448519b480f869208981a28ff73938701
吴新恒 <17629957093@163.com>
2022-10-18 07:50:28 +0800

    暂存单元测试代码

--- src/TDesign.Test/Components/InputNumberTest.cs
+++ src/TDesign.Test/Components/InputNumberTest.cs
@@ -1,4 +1,9 @@
-﻿namespace TDesign.Test.Components;
+﻿using AngleSharp.Common;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json.Linq;
+using System.Linq.Expressions;
+
+namespace TDesign.Test.Components;
 public class InputNumberTest : TestBase
 {
     IRenderedComponent<TInputNumber<TValue>> GetInputNumer<TValue>(Action<ComponentParameterCollectionBuilder<TInputNumber<TValue>>> parameterBuilder)
@@ -7,21 +12,32 @@ public class InputNumberTest : TestBase
     IRenderedComponent<TInputNumber<TValue>> GetInputNumer<TValue>(params ComponentParameter[] parameters)
         => TestContext.RenderComponent<TInputNumber<TValue>>(parameters);
 
-    [Fact(DisplayName = "InputNumber - 渲染组件")]
-    public void Test_Render_Int()
-    {
-        GetInputNumer<int>().MarkupMatches(@"
-<div class=""t-input-number t-size-m t-input-number--row"">
-    <button type=""button"" class=""t-button t-size-m t-button--variant-outline t-button--theme-default t-button--shape-square t-input-number__decrease"">
-        <i class=""t-icon t-icon-remove"" />
-    </button>
-    <div class=""t-input__wrap"">
-        <div class=""t-input t-size-m t-is-default t-align-center""><input autocomplete=""off"" placeholder=""请输入"" type=""text"" unselectable=""off"" class=""t-input__inner""></div>
-    </div>
-    <button type=""button"" class=""t-button t-size-m t-button--variant-outline t-button--theme-default t-button--shape-square t-input-number__increase"">
-        <i class=""t-icon t-icon-add"" />
-    </button>
-</div>
-");
-    }
+//    [Fact(DisplayName = "InputNumber - 渲染组件")]
+//    public void Test_Render_Int()
+//    {
+//        int value = 1;
+//        GetInputNumer<int>(x =>
+//        {
+//            x.Add(c => c.Value, 1);
+//            //x.Add(c => c.ValueExpression,);
+//            //x.Add(c => c.ValueChanged,);
+//        }).MarkupMatches(@"
+//<div class=""t-input-number t-size-m t-input-number--row"">
+//    <button type=""button"" class=""t-button t-size-m t-button--variant-outline t-button--theme-default t-button--shape-square t-input-number__decrease"">
+//        <i class=""t-icon t-icon-remove"" />
+//    </button>
+//    <div class=""t-input__wrap"">
+//        <div class=""t-input t-size-m t-is-default t-align-center""><input autocomplete=""off"" placeholder=""请输入"" type=""text"" unselectable=""off"" class=""t-input__inner""></div>
+//    </div>
+//    <button type=""button"" class=""t-button t-size-m t-button--variant-outline t-button--theme-default t-button--shape-square t-input-number__increase"">
+//        <i class=""t-icon t-icon-add"" />
+//    </button>
+//</div>
+//");
+//    }
+    //[Fact(DisplayName = "InputNumber - 测试")]
+    //public void Test()
+    //{
+    //    GetInputNumer<int>().Should().HaveTag("div");
+    //}
 }
