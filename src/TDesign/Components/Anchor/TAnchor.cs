d699b2b83704285939a202b537fbda08d22d0fb3
吴新恒 <17629957093@163.com>
2022-10-18 21:13:38 +0800

    新增 Anchor 组件

---
+++ src/TDesign/Components/Anchor/TAnchor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDesign
+{
+    [HtmlTag("div")]
+    [CssClass("t-anchoriz")]
+    public class TAnchor : BlazorComponentBase
+    {
+        protected override void AddContent(RenderTreeBuilder builder, int sequence)
+        {
+            builder.CreateElement(sequence + 1, "div", line => {
+
+                line.CreateElement(sequence + 2, "div", wrapper => {
+
+                    wrapper.CreateElement(sequence + 3, "div",attributes: new { 
+                        @class = "t-anchor__line-cursor",
+                        style= "top: 24px; height: 24px; opacity: 1;" });
+
+                }, new { @class = "t-anchor__line-cursor-wrapper" });
+
+            }, new { @class = "t-anchor__line" });
+        }
+    }
+}
---
+++ src/TDesign/Components/Anchor/TAnchorItem.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDesign
+{
+    [HtmlTag("div")]
+    [CssClass("t-anchor__item")]
+    public class TAnchorItem : BlazorComponentBase
+    {
+        /// <summary>
+        /// 锚点
+        /// </summary>
+        [Parameter] public string? Href { get; set; }
+        /// <summary>
+        /// 标题
+        /// </summary>
+        [Parameter] public string? Title { get; set; }
+
+        protected override void AddContent(RenderTreeBuilder builder, int sequence)
+        {
+            //builder.CreateComponent<TLink>(sequence+1,x=);
+        }
+    }
+}
