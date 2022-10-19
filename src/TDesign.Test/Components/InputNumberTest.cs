fdc8446ddbc41902d150efbe64ef04ecdccd80be
吴新恒 <17629957093@163.com>
2022-10-19 22:49:32 +0800

    更新单元测试

--- src/TDesign.Test/Components/InputNumberTest.cs
+++ src/TDesign.Test/Components/InputNumberTest.cs
@@ -26,22 +26,11 @@ public class InputNumberTest : TestBase
             x.Add(p => p.ValueExpression, () => value);
         })
         .Should()
-        .HaveClass("t-input-number")
+        .HaveTag("div")
+        .And.HaveClass("t-input-number")
         .And.HaveClass("t-size-m")
-        .And.HaveClass("t-input-number--row");
-        //        .MarkupMatches(@"
-        //<div class=""t-input-number t-size-m t-input-number--row"">
-        //    <button type=""button"" class=""t-button t-size-m t-button--variant-outline t-button--theme-default t-button--shape-square t-input-number__decrease"">
-        //        <i class=""t-icon t-icon-remove"" />
-        //    </button>
-        //    <div class=""t-input__wrap"">
-        //        <div class=""t-input t-size-m t-is-default t-align-center""><input autocomplete=""off"" placeholder=""请输入"" type=""text"" unselectable=""off"" class=""t-input__inner""></div>
-        //    </div>
-        //    <button type=""button"" class=""t-button t-size-m t-button--variant-outline t-button--theme-default t-button--shape-square t-input-number__increase"">
-        //        <i class=""t-icon t-icon-add"" />
-        //    </button>
-        //</div>
-        //");
+        .And.HaveClass("t-input-number--row")
+        .And.HaveChildMarkup("");
     }
     [Fact(DisplayName = "InputNumber - Size")]
     public void Test_Render_Parameter_Size()
