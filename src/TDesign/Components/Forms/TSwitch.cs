66a3960e291bcdeb02cc88622d6f4f9c6c469275
Elvis <elvismile@qq.com>
2022-10-20 17:14:22 +0800

    [perf] TSwitch组件的消除null警告

--- src/TDesign/Components/Forms/TSwitch.cs
+++ src/TDesign/Components/Forms/TSwitch.cs
@@ -24,7 +24,7 @@ public class TSwitch : BlazorInputComponentBase<bool>, IHasDisabled
     [Parameter][CssClass] public Size Size { get; set; } = Size.Medium;
 
     /// <summary>
-    ///  执行当 <see cref="TSwitch"/> 触发的事件。
+    ///  执行当 <see cref="TSwitch"/> 的值发生改变时的事件。
     /// </summary>
     [Parameter] public EventCallback<bool> OnChange { get; set; }
 
@@ -34,7 +34,7 @@ public class TSwitch : BlazorInputComponentBase<bool>, IHasDisabled
     [Parameter] public RenderFragment? TrueContent { get; set; }
 
     /// <summary>
-    /// 开关关闭时显示的自定义内容。
+    /// 开关关闭时，显示的自定义内容。
     /// </summary>
     [Parameter] public RenderFragment? FalseContent { get; set; }
 
@@ -45,20 +45,25 @@ public class TSwitch : BlazorInputComponentBase<bool>, IHasDisabled
     /// <param name="sequence"></param>
     protected override void AddContent(RenderTreeBuilder builder, int sequence)
     {
-
         builder.CreateElement(++sequence, "span",
-            span =>
-            {
-                span.CreateComponent<TIcon>(++sequence, attributes: new { Name = IconName.Loading }, condition: Loading);
-            },
-            attributes: new
-            {
-                @class = HtmlHelper.CreateCssBuilder().Append("t-switch__handle").Append(LOADING_CLASS_NAME, Loading).Append(DISABLED_CLASS_NAME, Disabled)
-            });
-        builder.CreateElement(++sequence, "div", childContent: Value ? TrueContent : FalseContent, attributes: new
+        span =>
         {
-            @class = HtmlHelper.CreateCssBuilder().Append("t-switch__content").Append(Size.GetCssClass()).Append(DISABLED_CLASS_NAME, Disabled)
+            span.CreateComponent<TIcon>(++sequence, attributes: new { Name = IconName.Loading }, condition: Loading);
+        },
+        attributes: new
+        {
+            @class = HtmlHelper.CreateCssBuilder().Append("t-switch__handle").Append(LOADING_CLASS_NAME, Loading).Append(DISABLED_CLASS_NAME, Disabled)
         });
+        var content = Value ? TrueContent : FalseContent;
+        var cssBuilder = HtmlHelper.CreateCssBuilder().Append("t-switch__content").Append(Size.GetCssClass()).Append(DISABLED_CLASS_NAME, Disabled);
+        if (content == null)
+        {
+            builder.CreateElement(++sequence, "div", attributes: new { @class = cssBuilder });
+        }
+        else
+        {
+            builder.CreateElement(++sequence, "div", childContent: content, attributes: new { @class = cssBuilder });
+        }
 
     }
 
@@ -87,12 +92,12 @@ public class TSwitch : BlazorInputComponentBase<bool>, IHasDisabled
     /// <param name="builder"></param>
     protected override void BuildCssClass(ICssClassBuilder builder)
     {
-        AppendClass(builder,CHECKED_CLASS_NAME,CurrentValue);
+        AppendClass(builder, CHECKED_CLASS_NAME, CurrentValue);
         AppendClass(builder, LOADING_CLASS_NAME, Loading);
         AppendClass(builder, DISABLED_CLASS_NAME, Disabled);
     }
 
-    private void AppendClass(ICssClassBuilder builder, string name, bool condition)
+    private static void AppendClass(ICssClassBuilder builder, string name, bool condition)
     {
         if (builder.Contains(name) && !condition)
         {
