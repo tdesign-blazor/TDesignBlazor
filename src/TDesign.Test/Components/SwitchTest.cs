ad6d04af20958e90faafcbea06f21194cc6b2ede
Elvis <elvismile@qq.com>
2022-10-23 19:34:28 +0800

    [perf] 完善Switch剩余属性的UT代码

--- src/TDesign.Test/Components/SwitchTest.cs
+++ src/TDesign.Test/Components/SwitchTest.cs
@@ -1,9 +1,4 @@
 ﻿using ComponentBuilder;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TDesign.Test.Components
 {
@@ -15,17 +10,8 @@ namespace TDesign.Test.Components
             var value = false;
             GetComponent(op =>
             {
-                op.Bind(item => item.Value, value, (switchValue) => { }
-                );
-            }).Should().HaveClass("t-switch")
-            .And
-            .HaveChildMarkup(
-                builder =>
-                    {
-                        builder.CreateElement(0, "span", attributes: new { @class = "t-switch__handle" });
-                        builder.CreateElement(1, "div", attributes: new { @class = "t-switch__content t-size-m" });
-                    }
-             );
+                op.Bind(item => item.Value, value, (switchValue) => { });
+            }).MarkupMatches("<div class=\"t-switch t-size-m\" placeholder=\"\"><span class=\"t-switch__handle\"></span><div class=\"t-switch__content t-size-m\"></div></div>");
         }
 
         [Fact(DisplayName = "Switch - Value 参数")]
@@ -36,10 +22,7 @@ namespace TDesign.Test.Components
             {
                 op.Bind(item => item.Value, value, (switchValue) => { }
                 );
-            }).Should()
-            .HaveClass("t-switch")
-            .And
-            .HaveClass("t-is-checked");
+            }).Should().HaveClass("t-switch").And.HaveClass("t-is-checked");
         }
 
         [Fact(DisplayName = "Switch - Disabled 参数")]
@@ -50,10 +33,7 @@ namespace TDesign.Test.Components
             {
                 op.Bind(item => item.Value, value, (switchValue) => { });
                 op.Add(item => item.Disabled, true);
-            }).Should()
-            .HaveClass("t-is-disabled")
-            .And
-            .HaveChildMarkup(builder => builder.CreateElement(0, "span", attributes: new { @class = "t-switch__handle t-is-disabled" }));
+            }).Should().HaveClass("t-is-disabled").And.HaveChildMarkup(builder => builder.CreateElement(0, "span", attributes: new { @class = "t-switch__handle t-is-disabled" }));
         }
 
         [Fact(DisplayName = "Switch - Loading 参数")]
@@ -72,6 +52,51 @@ namespace TDesign.Test.Components
                     builder.CreateElement(0, "span", child => child.CreateElement(1, "i", attributes: new { @class = "t-icon t-icon-loading" }),
                 attributes: new { @class = "t-switch__handle t-is-loading" })
              );
+        }
+
+        [Fact(DisplayName = "Switch - Size 参数")]
+        public void Test_Size_Parameter()
+        {
+            var value = false;
+            GetComponent(op =>
+            {
+                op.Bind(item => item.Value, value, (switchValue) => { });
+                op.Add(item => item.Size, Size.Small);
+            }).Should().HaveClass("t-size-s");
+
+            GetComponent(op =>
+            {
+                op.Bind(item => item.Value, value, (switchValue) => { });
+                op.Add(item => item.Size, Size.Medium);
+            }).Should().HaveClass("t-size-m");
+
+            GetComponent(op =>
+            {
+                op.Bind(item => item.Value, value, (switchValue) => { });
+                op.Add(item => item.Size, Size.Large);
+            }).Should().HaveClass("t-size-l");
+        }
+
+        [Fact(DisplayName = "Switch - TrueContent 参数")]
+        public void Test_TrueContent_Parameter()
+        {
+            var value = true;
+            GetComponent(op =>
+            {
+                op.Bind(item => item.Value, value, (switchValue) => { });
+                op.Add(item => item.TrueContent, builder => { builder.AddContent(0, "OPEN"); });
+            }).MarkupMatches("<div class=\"t-switch t-size-m t-is-checked\" placeholder=\"\" blazor:onclick=\"1\"><span class=\"t-switch__handle\"></span><div class=\"t-switch__content t-size-m\">OPEN</div></div>");
+        }
+
+        [Fact(DisplayName = "Switch - FalseContent 参数")]
+        public void Test_FalseContent_Parameter()
+        {
+            var value = false;
+            GetComponent(op =>
+            {
+                op.Bind(item => item.Value, value, (switchValue) => { });
+                op.Add(item => item.FalseContent, builder => { builder.AddContent(0, "CLOSE"); });
+            }).MarkupMatches("<div class=\"t-switch t-size-m\" placeholder=\"\" blazor:onclick=\"1\"><span class=\"t-switch__handle\"></span><div class=\"t-switch__content t-size-m\">CLOSE</div></div>");
         }
     }
 }
