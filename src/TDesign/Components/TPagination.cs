b1d937174ace4a6e05d3e3156c2b2469a109b9a8
William <william-zzh@outlook.com>
2022-11-18 19:10:16 +0800

    feat: 条件转 feat: 极简版参数 feat: 修改 Smal 为 Size

--- src/TDesign/Components/TPagination.cs
+++ src/TDesign/Components/TPagination.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components.Rendering;
+﻿using System.Linq.Expressions;
+
+using Microsoft.AspNetCore.Components.Rendering;
 
 namespace TDesign;
 /// <summary>
@@ -11,7 +13,7 @@ public class TPagination : BlazorComponentBase
     /// <summary>
     /// 设置分页组件的大小。
     /// </summary>
-    [Parameter][BooleanCssClass("t-size-s", "t-size-m")] public bool Small { get; set; }
+    [Parameter][CssClass] public Size Size { get; set; } = Size.Medium;
 
     #region Current
     /// <summary>
@@ -54,6 +56,7 @@ public class TPagination : BlazorComponentBase
     [Parameter] public bool ShowTotal { get; set; } = true;
     #endregion
 
+    #region PageNumber
     /// <summary>
     /// 设置分页页码条的显示数量。建议范围在 5-21 之间，默认是 7。
     /// </summary>
@@ -61,7 +64,29 @@ public class TPagination : BlazorComponentBase
     /// <summary>
     /// 设置是否显示页码条。
     /// </summary>
-    [Parameter]public bool ShowPageNumber { get; set; }
+    [Parameter] public bool ShowPageNumber { get; set; } = true;
+    #endregion
+
+    /// <summary>
+    /// 是否显示首页和末页按钮。
+    /// </summary>
+    [Parameter] public bool ShowFirstLastBtn { get; set; }
+
+    /// <summary>
+    /// 设置当页码数量超出时，前后省略模式。
+    /// </summary>
+    [Parameter] public PageEllipsisMode EllipsisMode { get; set; } = PageEllipsisMode.Middle;
+
+    #region JumpPage
+    /// <summary>
+    /// 设置是否显示跳转到文本框。
+    /// </summary>
+    [Parameter] public bool ShowJumpPage { get; set; }
+    /// <summary>
+    /// 设置为极简版的分页。
+    /// </summary>
+    [Parameter] public bool Simple { get; set; }
+    #endregion
     #endregion
 
 
@@ -87,6 +112,10 @@ public class TPagination : BlazorComponentBase
             return result;
         }
     }
+    /// <summary>
+    /// 跳转页码。
+    /// </summary>
+    long JumpPage { get; set; } = 1;
     #endregion
 
     #region Method
@@ -102,7 +131,9 @@ public class TPagination : BlazorComponentBase
         page = page < 1 ? 1 : page;
         page = page > TotalPages ? TotalPages : page;
 
+
         Current = page;
+        JumpPage = page;
         await CurrentChanged.InvokeAsync(page);
         await this.Refresh();
     }
@@ -146,17 +177,31 @@ public class TPagination : BlazorComponentBase
         TotalContent ??= value => new RenderFragment(builder => builder.AddContent(0, $"共 {Total} 项数据"));
     }
 
+    /// <inheritdoc/>
     protected override void AddContent(RenderTreeBuilder builder, int sequence)
     {
         BuildTotal(builder, sequence);
+        BuildPageSizeSelect(builder, sequence + 1);
 
-        //上一页
-        BuildPrevOrNextBtn(builder, sequence + 1, true, Current <= 1);
+        BuildPageBehaviorBtn(builder, sequence + 10, PageButtonBehavior.First, IconName.PageFirst, ShowFirstLastBtn, Current <= 1);
+        BuildPageBehaviorBtn(builder, sequence + 11, PageButtonBehavior.Previous, IconName.ChevronLeft, disabled: Current <= 1);
 
-        BuildPageNumbers(builder, sequence + 2);
+        if (Simple)
+        {
+            BuildJumper(builder, sequence + 20);
+        }
+        else
+        {
+            BuildPageNumbers(builder, sequence + 20);
+        }
 
-        //下一页
-        BuildPrevOrNextBtn(builder, sequence + 3, false, Current >= TotalPages);
+        BuildPageBehaviorBtn(builder, sequence + 21, PageButtonBehavior.Next, IconName.ChevronRight, disabled: Current >= TotalPages);
+        BuildPageBehaviorBtn(builder, sequence + 22, PageButtonBehavior.Last, IconName.PageLast, ShowFirstLastBtn, Current >= TotalPages);
+
+        if (!Simple)
+        {
+            BuildJumper(builder, sequence + 30);
+        }
     }
     #endregion
 
@@ -168,34 +213,44 @@ public class TPagination : BlazorComponentBase
         => builder.CreateElement(sequence, "div", TotalContent?.Invoke(Total), new { @class = "t-pagination__total" }, ShowTotal);
 
     /// <summary>
-    /// 构建上一页或下一页按钮。
-    /// <param name="prevOrNext"><c>true</c> 表示上一页，否则是下一页。</param>
-    /// <param name="disabled">是否被禁用。</param>
-    void BuildPrevOrNextBtn(RenderTreeBuilder builder, int sequence, bool prevOrNext, bool disabled = default)
+    /// 构建每页数据量的下拉菜单。//TODO，等待 Select 组件完成
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <param name="sequence"></param>
+    void BuildPageSizeSelect(RenderTreeBuilder builder, int sequence)
     {
         builder.CreateElement(sequence, "div", content =>
         {
-            content.CreateComponent<TIcon>(0, attributes: new { Name = prevOrNext ? IconName.ChevronLeft : IconName.ChevronRight });
+            content.AddContent(0, $"{PageSize} 条/页");
+        }, new { @class = "t-select__wrap t-pagination__select" });
+    }
+
+    /// <summary>
+    /// 构建上一页或下一页按钮。
+    /// <param name="prevOrNext"><c>true</c> 表示上一页，否则是下一页。</param>
+    /// <param name="disabled">是否被禁用。</param>
+    void BuildPageBehaviorBtn(RenderTreeBuilder builder, int sequence, PageButtonBehavior behavior, object iconName, bool show = true, bool disabled = default)
+    {
+        builder.CreateElement(sequence, "div", content =>
+        {
+            content.CreateComponent<TIcon>(0, attributes: new { Name = iconName });
         }, new
         {
             @class = HtmlHelper.CreateCssBuilder()
                                 .Append("t-pagination__btn")
-                                .Append("t-pagination__btn-prev", prevOrNext)
-                                .Append("t-pagination__btn-next", !prevOrNext)
+                                .Append("t-pagination__btn-prev", behavior is PageButtonBehavior.First or PageButtonBehavior.Previous)
+                                .Append("t-pagination__btn-next", behavior is PageButtonBehavior.Next or PageButtonBehavior.Last)
                                 .Append("t-is-disabled", disabled)
                                 ,
-            onclick = HtmlHelper.CreateCallback(this, async () =>
+            onclick = HtmlHelper.CreateCallback(this, () => behavior switch
             {
-                if (prevOrNext)//上一页
-                {
-                    await NavigateToPrevious();
-                }
-                else
-                {
-                    await NavigateToNext();
-                }
-            }, !disabled)
-        });
+                PageButtonBehavior.First => NavigateToFirst(),
+                PageButtonBehavior.Last => NavigateToLast(),
+                PageButtonBehavior.Previous => NavigateToPrevious(),
+                _ => NavigateToNext()
+            }
+            , !disabled)
+        }, show);
     }
 
     /// <summary>
@@ -219,7 +274,10 @@ public class TPagination : BlazorComponentBase
             disabled
         });
     }
-
+    /// <summary>
+    /// 构建分页数字。
+    /// </summary>
+    /// <param name="pageNumber">分页数字。</param>
     void BuildPageNumerItem(RenderTreeBuilder builder, int sequence, long pageNumber)
     {
         if (pageNumber == Current)
@@ -237,28 +295,30 @@ public class TPagination : BlazorComponentBase
     /// </summary>
     void BuildPageNumbers(RenderTreeBuilder builder, int sequence)
     {
-
         //分页条
         builder.CreateElement(sequence + 1, "ul", content =>
         {
-            #region 第一页
-
-            //第一页永远显示
-            BuildPageNumerItem(content, 0, 1);
-
-            #endregion
-
-            #region 前5页
-            if (Current > PageNumber / 2)
+            if (EllipsisMode == PageEllipsisMode.Middle)
             {
-                var backTo = PageNumber - 5;
-                if (backTo <= 1)
+                #region 第一页
+
+                //第一页永远显示
+                BuildPageNumerItem(content, 0, 1);
+
+                #endregion
+
+                #region 前5页
+                if (Current > PageNumber / 2)
                 {
-                    backTo = 1;
+                    var backTo = PageNumber - 5;
+                    if (backTo <= 1)
+                    {
+                        backTo = 1;
+                    }
+                    BuildPageItem(content, 1, text => text.CreateComponent<TIcon>(0, attributes: new { Name = IconName.Ellipsis }), () => NavigateToPage(backTo));
                 }
-                BuildPageItem(content, 1, text => text.CreateComponent<TIcon>(0, attributes: new { Name = IconName.Ellipsis }), () => NavigateToPage(backTo));
+                #endregion
             }
-            #endregion
 
             #region 页码条
 
@@ -266,8 +326,8 @@ public class TPagination : BlazorComponentBase
 
             //页码1 永远显示，所有从2开始
             //最后一页永远显示，所以结束要少一个索引
-
-            for (long i = start + 1; i <= end - 1; i++)
+            var offset = (EllipsisMode == PageEllipsisMode.Middle ? 1 : 0);
+            for (long i = start + offset; i <= end - offset; i++)
             {
                 var current = i;
                 var contentSequence = (int)i + 30;
@@ -276,21 +336,24 @@ public class TPagination : BlazorComponentBase
             }
             #endregion
 
-            #region 后5页
-            if (Current < TotalPages - PageNumber / 2)
+            if (EllipsisMode == PageEllipsisMode.Middle)
             {
-                var nextTo = Current + 5;
-                if (nextTo >= TotalPages)
+                #region 后5页
+                if (Current < TotalPages - PageNumber / 2)
                 {
-                    nextTo = TotalPages;
+                    var nextTo = Current + 5;
+                    if (nextTo >= TotalPages)
+                    {
+                        nextTo = TotalPages;
+                    }
+                    BuildPageItem(content, 90, text => text.CreateComponent<TIcon>(0, attributes: new { Name = IconName.Ellipsis }), () => NavigateToPage(nextTo));
                 }
-                BuildPageItem(content, 90, text => text.CreateComponent<TIcon>(0, attributes: new { Name = IconName.Ellipsis }), () => NavigateToPage(nextTo));
-            }
-            #endregion
+                #endregion
 
-            #region 末页
-            BuildPageNumerItem(content, 100, TotalPages);
-            #endregion
+                #region 末页
+                BuildPageNumerItem(content, 100, TotalPages);
+                #endregion
+            }
 
         }, new { @class = "t-pagination__pager" }, ShowPageNumber);
 
@@ -330,7 +393,78 @@ public class TPagination : BlazorComponentBase
             return (start, end);
         }
     }
+
+    /// <summary>
+    /// 构建 跳转到 文本框。
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <param name="sequence"></param>
+    void BuildJumper(RenderTreeBuilder builder, int sequence)
+    {
+        builder.CreateElement(sequence, "div", content =>
+        {
+            content.AddContent(0, "跳至");
+            content.CreateComponent<TInputAdornment>(1, input =>
+            {
+                input.CreateComponent<TInputNumber<long>>(0, attributes: new
+                {
+                    AdditionalCssClass = "t-pagination__input",
+                    Theme = InputNumberTheme.Normal,
+                    Min = 1L,
+                    Max = TotalPages,
+                    Size,
+                    Value = JumpPage,
+                    ValueExpression = (Expression<Func<long>>)(() => JumpPage),
+                    ValueChanged = HtmlHelper.CreateCallback<long>(this, value =>
+                    {
+                        NavigateToPage(value);
+                    })
+                });
+            }, new
+            {
+                Append = $"/{TotalPages} 页"
+            });
+        }, new { @class = "t-pagination__jump" }, ShowJumpPage);
+    }
     #endregion
 
     #endregion
+
+    /// <summary>
+    /// 分页按钮的行为。
+    /// </summary>
+    enum PageButtonBehavior
+    {
+        /// <summary>
+        /// 首页。
+        /// </summary>
+        First,
+        /// <summary>
+        /// 上一页。
+        /// </summary>
+        Previous,
+        /// <summary>
+        /// 下一页。
+        /// </summary>
+        Next,
+        /// <summary>
+        /// 末页。
+        /// </summary>
+        Last
+    }
+}
+
+/// <summary>
+/// 页码条省略模式。
+/// </summary>
+public enum PageEllipsisMode
+{
+    /// <summary>
+    /// 中间省略。
+    /// </summary>
+    Middle,
+    /// <summary>
+    /// 两端省略。
+    /// </summary>
+    BothEnds
 }
