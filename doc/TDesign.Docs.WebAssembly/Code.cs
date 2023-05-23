fad3ad4dc928f5f096b2fa08882173a779d338df
Zhou Zihao <william-zzh@outlook.com>
2023-05-23 13:51:51 +0800

    refactor(docs): 使用后端托管前端优化文档项目

---
+++ doc/TDesign.Docs.WebAssembly/Code.cs
@@ -0,0 +1,13 @@
+﻿using Markdig;
+using Microsoft.AspNetCore.Components;
+
+namespace TDesign.Docs.WebAssembly;
+public static class Code
+{
+    public static MarkupString Create(string value)
+    {
+        var content = Markdown.ToHtml(value);
+        return new MarkupString(content);
+    }
+    public const string BgRun = "background:#ccc";
+}
--- doc/TDesign.Docs.WebAssembly/Program.cs
+++ doc/TDesign.Docs.WebAssembly/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-using TDesign.Docs.Shared;
+using TDesign.Docs.WebAssembly;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
