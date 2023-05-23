031340d5e1baaf9f83afe8c341ea439823dd03d3
Zhou Zihao <william-zzh@outlook.com>
2023-05-23 17:36:47 +0800

    调整暗黑代码

--- src/TDesign/wwwroot/tdesign-blazor.js
+++ src/TDesign/wwwroot/tdesign-blazor.js
@@ -1,30 +1,4 @@
 
-let tdesign = {
-    /**
-     * 为指定的元素订阅js事件，该方法可支持保留原有的事件不被覆盖，并支持参数指定强制覆盖。
-     * @param {Element} element 要注册的元素
-     * @param {String} name 事件名称，如 "onscroll"
-     * @param {Function} handler 事件方法
-     * @param {Boolean} replace 是否覆盖原有事件，默认为false，需要覆盖时传入true
-     */
-    setEvent: function(element, name, handler, replace = false) {
-        let functionStored = element[name];
-        element[name] = function(e) {
-            if (!replace && functionStored) {
-                functionStored(e)
-            }
-            handler(e);
-        }
-    },
-    focus: function (element,dotnetHelper) {
-        if (element) {
-            element.focus();
-            if (dotnetHelper) {
-                dotnetHelper.invokeMethodAsync("Invoke");
-            }
-        }
-    }
-}
 ///**
 // * @description 组件 affix 用到的js对象。
 // */
