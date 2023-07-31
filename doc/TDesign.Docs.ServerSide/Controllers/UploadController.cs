2f23fe2eccd8a59c89c025d3261eb7d56c8feccd
Zhou Zihao <william-zzh@outlook.com>
2023-07-31 18:27:05 +0800

    修复后端访问的服务和中间件

--- doc/TDesign.Docs.ServerSide/Controllers/UploadController.cs
+++ doc/TDesign.Docs.ServerSide/Controllers/UploadController.cs
@@ -9,18 +9,20 @@ public class UploadController:ControllerBase
     [HttpPost("file")]
     public async Task<IActionResult> PostAsync([FromForm]List<IFormFile> files)
     {
-        //var savedPath = "/upload";
-        //var generateFileName = Path.Combine(DateTimeOffset.UnixEpoch.ToString(), Path.GetExtension(file.Name));
+        var savedPath = "/upload";
 
-        //if ( !Directory.Exists(savedPath) )
-        //{ 
-        //    Directory.CreateDirectory(savedPath);
-        //}
+        if ( !Directory.Exists(savedPath) )
+        {
+            Directory.CreateDirectory(savedPath);
+        }
+        foreach ( var file in files )
+        {
+            var generateFileName = Path.Combine(DateTimeOffset.UnixEpoch.ToString(), Path.GetExtension(file.FileName));
+            var serverFilePath = Path.Combine(savedPath, generateFileName);
 
-        //var serverFilePath = Path.Combine(savedPath, generateFileName);
-
-        //using var fileStream = new FileStream(serverFilePath, FileMode.CreateNew);
-        //await file.OpenReadStream().CopyToAsync(fileStream);
+            using var fileStream = new FileStream(serverFilePath, FileMode.CreateNew);
+            await file.OpenReadStream().CopyToAsync(fileStream);
+        }
 
         return Ok();
     }
