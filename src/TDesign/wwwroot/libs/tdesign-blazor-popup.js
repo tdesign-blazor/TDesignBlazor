6c52c5c6c26ac3542bfd8a81e2294e049d145c0a
Zhou Zihao <william-zzh@outlook.com>
2023-08-08 18:46:19 +0800

    fix: TPopup 的闪退和位置问题

--- src/TDesign/wwwroot/libs/tdesign-blazor-popup.js
+++ src/TDesign/wwwroot/libs/tdesign-blazor-popup.js
@@ -4,34 +4,71 @@
  * 弹出层
  * */
 let popup = {
+    animationClass: {
+        ANIMATION_ENTER: 't-popup--animation-enter',
+        ANIMATION_ENTER_FROM: 't-popup--animation-enter-from',
+        ANIMATION_EXITING: 't-popup--animation-exiting',
+        ANIMATION_LEAVE_TO: 't-popup--animation-leave-to',
+        ANIMATION_ENTER_TO: 't-popup--animation-enter-to',
+        ANIMATION_ENTERING: 't-popup--animation-entering',
+        ANIMATION_LEAVE_FROM: 't-popup--animation-leave-from',
+        ANIMATION_LEAVE: 't-popup--animation-leave',
+        ANIMATION_ENTER_ACTIVE: 't-popup--animation-enter-active',
+        ANIMATION_LEAVE_ACTIVE: 't-popup--animation-leave-active',
+    },
+    popperList: [],
     /**
      * 创建一个新的弹出层引用
-     * @param objRef 触发对象的引用
-     * @param popupRef 弹出提示对象的引用
+     * @param trigerElement 触发对象的引用
+    * @param popupElement 弹出提示对象的引用
      * @param options 弹出层配置
      * @param optionDotNetHelper options 包装的 DotNetReference 对象
      * @returns popper 对象
-     * */
-    show: function (trigerElement, popupElement, options, optionDotNetReference,dotNetHelper) {
+    * */
+    show: function (trigerElement, popupElement, options, dotNetHelper) {
 
+        let popper;
         options.onFirstUpdate = state => {
-            optionDotNetReference.invokeMethodAsync("InvokeOnFirstUpdate", { placement: state.placement });
+            console.debug('placement:' + state.placement);
+            //optionDotNetReference.invokeMethodAsync("InvokeOnFirstUpdate", { placement: state.placement });
         }
-        let popper = createPopper(trigerElement, popupElement, options);
+        setTimeout(() => {
+            popper = createPopper(trigerElement, popupElement, options);
+            document.body.appendChild(popupElement);
 
+            window.addEventListener("click", () => {
+                this.hide(popupElement, dotNetHelper);
+            }, {once:true});
 
-        window.addEventListener("click", () => {
-            dotNetHelper.invokeMethodAsync("Invoke");
-        });
+            //popupElement.style.display = "";
+            popupElement.style.display = "";
+            popupElement.classList.remove(this.animationClass.ANIMATION_ENTER, this.animationClass.ANIMATION_LEAVE_ACTIVE);
+            popupElement.classList.add(this.animationClass.ANIMATION_ENTER_ACTIVE, this.animationClass.ANIMATION_LEAVE);
+            dotNetHelper.invokeMethodAsync('onShown');
 
-        document.body.appendChild(popupElement);
-        popupElement.style.display = "";
-        return popper;
+            this.popperList[popupElement] = popper;
+        }, 400);
+        //return popper;
     },
-    hide: function (popper, popupElement, options) {
-        if (popper) {
-            popper.destroy();
+    hide: function (popupElement, dotNetHelper) {
+        if (popupElement) {
+
+            const popper = this.popperList[popupElement];
+            if (!popper) {
+                return;
+            }
+            popupElement.classList.remove(this.animationClass.ANIMATION_LEAVE, this.animationClass.ANIMATION_ENTER_ACTIVE);
+            popupElement.classList.add(this.animationClass.ANIMATION_LEAVE_ACTIVE, this.animationClass.ANIMATION_ENTER);
+
+            setTimeout(() => {
+                popper.destroy();
+                popupElement.style.display = 'none';
+                dotNetHelper.invokeMethodAsync('onHidden');
+            },400)
         }
+    },
+    clear: () => {
+        this.popperList.clear();
     }
 }
 
